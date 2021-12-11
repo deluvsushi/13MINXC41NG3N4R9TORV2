9844e8c5f220ac2d0b06822b26c7e3d466ad61c7
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-12-11 11:30:22 +0600

    Create main_functions.py

---
+++ coin_generator_configs/main_functions.py
@@ -0,0 +1,36 @@
+from json import load
+from requests import post
+from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
+
+auids = [ ]
+with open("auids.json") as data:
+	auids_list = load(data)
+	for auid in auids_list:
+		auids.append(auid)
+
+		# -- coin generator functions --
+
+def get_tapjoy_reward(auid: str):
+	data = {"userId": auid, "repeat": "200"}
+	request = post(f"https://samino.sirlez.repl.co/api/tapjoy", json=data)
+	print(f"Generating Coins In {auid} - {request.text}") 
+
+def generating_process(auid: str):
+	Thread(target=get_tapjoy_reward, args=([auid])).start()
+
+def main_process():
+    for auid in auids:
+    	auid = auid["auid"]
+    	try:
+    		with ThreadPoolExecutor(max_workers=5) as executor: 
+    			_ = [executor.submit(generating_process, auid)]
+    	except Exception as e:
+    		print(f">> Error in main process - {e}")
+		
+def main():
+	print("1) Generate Coins")
+	select = int(input("Select >> "))
+	
+	if select == 1:
+		main_process()
