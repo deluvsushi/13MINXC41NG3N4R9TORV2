827b607d1a766aed01c4509586f1701af3386bb4
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-12-11 18:47:51 +0600

    Update AminoCoinGeneratorV2.py

--- AminoCoinGeneratorV2.py
+++ AminoCoinGeneratorV2.py
@@ -1,5 +1,53 @@
-from coin_generator_configs import main_functions, menu_configs
-print("""Script by deluvsushi
-Github : https://github.com/deluvsushi""")
-print(menu_configs.banner)
-main_functions.main()
+from json import load
+from requests import post
+from threading import Thread
+from coin_generator_configs import menu_configs
+from concurrent.futures import ThreadPoolExecutor
+
+auids = []
+with open("auids.json") as data:
+    auids_list = load(data)
+    for auid in auids_list:
+        auids.append(auid)
+
+        # -- coin generator functions --
+
+
+def get_tapjoy_reward(auid: str):
+    data = {"userId": auid, "repeat": "200"}
+    request = post(f"https://samino.sirlez.repl.co/api/tapjoy", json=data)
+    print(f"Generating Coins In {auid} - {request.text}")
+
+
+def generating_process(auid: str):
+    Thread(target=get_tapjoy_reward, args=([auid])).start()
+
+
+def main_process():
+    for auid in auids:
+        auid = auid["auid"]
+        try:
+            with ThreadPoolExecutor(max_workers=5) as executor:
+                _ = [executor.submit(generating_process, auid)]
+        except Exception as e:
+            print(f">> Error in main process - {e}")
+
+
+def main():
+    print(
+        """Script by deluvsushi
+Github : https://github.com/deluvsushi"""
+    )
+    print(menu_configs.banner)
+    select = int(
+        input(
+            """
+1) Generate Coins
+Select >> """
+        )
+    )
+
+    if select == 1:
+        main_process()
+
+main()
