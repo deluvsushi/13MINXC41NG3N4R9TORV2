92e053612bf58317db13278eea332170122417e5
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-12-11 11:29:52 +0600

    Create AminoCoinGeneratorV2.py

---
+++ AminoCoinGeneratorV2.py
@@ -0,0 +1,5 @@
+from coin_generator_configs import main_functions, menu_configs
+print("""Script by deluvsushi
+Github : https://github.com/deluvsushi""")
+print(menu_configs.banner)
+main_functions.main()
